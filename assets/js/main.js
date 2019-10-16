1f230b361d047c8649078bc1cd0e8be265363004
gnich12 <jinto12@gmail.com>
2019-10-15 22:34:54 -0700

    fix animation on footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,84 @@
 $(document).ready(function() {
     $('#env').on({
         click: function() {
-            console.log('hello world')
+            event.preventDefault();
+            if($('#phonenum').hasClass('active-mail')) {
+                $('#phonenum').removeClass('active-mail').addClass('disable-icons')
+                $('#mobi').removeClass('white').addClass('drk-blue')
+                $('#phonecircle').removeClass('drk-blue').addClass('white')
+                $('#email').removeClass('disable-icons').addClass('active-mail')
+                $('#envicon').removeClass('drk-blue').addClass('white')
+                $('#mailcircle').addClass('drk-blue')
+
+            } else {
+                $('#email').removeClass('disable-icons').addClass('active-mail')
+                $('#envicon').removeClass('drk-blue').addClass('white')
+                $('#mailcircle').addClass('drk-blue')
+            }
+            setTimeout(function(){
+                $('#email')
+                    .removeClass('active-mail')
+                    .addClass('disable-icons')
+                    .fadeOut('slow')
+                    $('#envicon').removeClass('white').addClass('drk-blue')
+                    $('#mailcircle').removeClass('drk-blue').addClass('white')
+            }, 10000)
         },
         mouseenter: function(event) {
             event.preventDefault();
-            $('#email').removeClass('disable-icons')
-            $('#email').addClass('active-mail')
-            $('#mailcircle').addClass('has-text-grey')
-
+            if(!$('#email').hasClass('active-mail')) {    
+                $('#envicon').removeClass('drk-blue').addClass('white')
+                $('#mailcircle').addClass('drk-blue')
+            }
         },
         mouseleave: function(event) {
             event.preventDefault();
-            $('#email').addClass('disable-icons')
-            $('#mailcircle').removeClass('has-text-grey')
+            if(!$('#email').hasClass('active-mail')) { 
+                $('#envicon').removeClass('white').addClass('drk-blue')
+                $('#mailcircle').removeClass('drk-blue').addClass('white')
+            }
         }
     })
     $('#phone').on({
         click: function() {
-            console.log('hello world')
+            event.preventDefault();
+            if($('#email').hasClass('active-mail')) {
+                $('#email').removeClass('active-mail').addClass('disable-icons')
+                $('#phonenum').removeClass('disable-icons').addClass('active-mail')
+                $('#mobi').removeClass('drk-blue').addClass('white')
+                $('#envicon').removeClass('white').addClass('drk-blue')
+                $('#mailcircle').removeClass('drk-blue').addClass('white')
+                $('#phonecircle').addClass('drk-blue')
+            } else {
+                $('#phonenum').removeClass('disable-icons').addClass('active-mail')
+                $('#mobi').removeClass('drk-blue').addClass('white')
+                $('#phonecircle').addClass('drk-blue')
+            }
+            setTimeout(function(){
+                $('#phonenum')
+                    .removeClass('active-mail')
+                    .addClass('disable-icons')
+                    .fadeOut('slow')
+                    $('#mobi').removeClass('white').addClass('drk-blue')
+                    $('#phonecircle').removeClass('drk-blue').addClass('white')
+            }, 10000)
         },
         mouseenter: function(event) {
             event.preventDefault();
-            $('#phonenum').removeClass('disable-icons')
-            $('#phonenum').addClass('active-phone')
-            $('#phonecircle').addClass('has-text-grey')
+            if(!$('#phonenum').hasClass('active-mail')) {    
+                $('#mobi').removeClass('drk-blue')
+                $('#mobi').addClass('white')
+                $('#phonecircle').addClass('drk-blue')
+            }
 
         },
         mouseleave: function() {
-            $('#phonenum').addClass('disable-icons')
-            $('#phonecircle').removeClass('has-text-grey')
+            event.preventDefault();
+            if(!$('#phonenum').hasClass('active-mail')) {    
+                $('#mobi').removeClass('white').addClass('drk-blue')
+                $('#phonecircle').removeClass('drk-blue').addClass('white')
+            }
         }
     })
+    
 })
