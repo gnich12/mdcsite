1bd880e036a66308afdbe39f03c6dd3827ad890d
gnich12 <jinto12@gmail.com>
2019-12-02 20:26:50 -0800

    update js file
    remove jquery mobile. solving loading bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,8 +85,8 @@ $(document).ready(function() {
             $('#envicon').removeAttr('style').attr('style', 'color:#3B5382')
         }
     }
-    $('#env-mobile').bind( "tap", tapHandler );
-    $('#env-phone').bind( "tap", tapHandler2 );
+    $('#env-mobile').on( "click", tapHandler );
+    $('#env-phone').on( "click", tapHandler2 );
     function tapHandler (event){
         event.preventDefault()
         clearTimeout(mobiphone)
